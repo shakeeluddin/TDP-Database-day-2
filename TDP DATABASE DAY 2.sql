a06701904f62165b61b453d15c75686f1d2554f0
unknown <you@example.com>
2022-09-21 12:32:40 +0100

    updated db

--- TDP DATABASE DAY 2.sql
+++ TDP DATABASE DAY 2.sql
@@ -9,8 +9,37 @@ SHOW TABLES;
 DESCRIBE CUSTOMERS;
 CREATE TABLE items(
 item_number INT UNIQUE NOT NULL AUTO_INCREMENT,
-iten_name VARCHAR(100)NOT NULL,
+item_name VARCHAR(100)NOT NULL,
 course_type VARCHAR(100) NOT NULL,
 Price_item DECIMAL NOT NULL,
 PRIMARY KEY (item_number));
+DROP TABLE items;
 SHOW TABLES;
+INSERT INTO customers(customer_name, phone_number) VALUES ("Shakeel","12345678912");
+SELECT * FROM customers;
+INSERT INTO customers(customer_name,phone_number) VALUES ("uddin","9876543212");
+SELECT * FROM customers;
+INSERT INTO customers(customer_name,phone_number) VALUES ("Adam","34567125679");
+SELECT * FROM customers;
+INSERT INTO customers(customer_name,phone_number) VALUES ("Zak", "84629539571");
+SELECT * FROM customers;
+INSERT INTO customers(customer_name,phone_number) VALUES ("Faaris","34975902847"); 
+SELECT * FROM customers;
+INSERT INTO items(item_name,course_type,price_item) VALUES ("Milk Cake", "Dessert", "3.99");
+SELECT * FROM items;
+INSERT INTO items(item_name,course_type,price_item) VALUES ("Pasta","mains","5");
+SELECT * FROM items;
+INSERT INTO items(item_name,course_type, price_item) VALUES ("Rice", "Mains","10");
+SELECT * FROM items;
+INSERT INTO items(item_name,course_type,price_item) VALUES ("Lamb chops", "Mains", "20");
+SELECT * FROM items;
+INSERT INTO items(item_name,course_type,price_item) VALUES ("Cake", "dessert", "5");
+SELECT * FROM items;
+UPDATE items SET item_name="cake",price_item="10" WHERE item_number=1;
+SELECT * FROM items;
+DELETE FROM items WHERE item_number=5; 
+SELECT * FROM items;
+INSERT INTO items(item_name,course_type,price_item) VALUES ("Yoghurt", "dessert", "6");
+SELECT * FROM items;
+
+
